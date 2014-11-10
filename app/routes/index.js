1300000f597f9b1ea696ed46198aa7d8de90bd7d
Michelle Santarsiero <michelle.santarsiero@gmail.com>
2014-11-10 14:31:01 -0500

    add styles

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return [
-      { description: 'first event' },
-      { description: 'second event' },
-      { description: 'third event' }
-    ]
+    this.transitionTo('events');
   }
 
 });
